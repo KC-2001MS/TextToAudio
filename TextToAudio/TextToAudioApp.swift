ab2eb017238004634f975b2dc160703292dfa676
KC-2001MS <iroiro.work1234@gmail.com>
2022-10-20 09:54:58 +0900

    Initial Commit

---
+++ TextToAudio/TextToAudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextToAudioApp.swift
+//  TextToAudio
+//
+//  Created by 茅根 啓介 on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct TextToAudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
